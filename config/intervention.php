780b72a3a33f13c4fdfd69b709be10e90c5385da
Clayton Collie <clayton.collie@gmail.com>
2022-06-03 18:17:22 -0500

    site title

--- config/intervention.php
+++ config/intervention.php
@@ -22,8 +22,8 @@ return [
 			'wordpress-seo/wp-seo.php'           => true,
 		],
 		'general'             => [
-			'site-title'   => 'JUMP',
-			'tagline'      => 'Into web3',
+			// 'site-title'   => 'JUMP',
+			// 'tagline'      => 'Into web3',
 			'admin-email'  => 'jeff@parachute.xyz',
 			'membership'   => true,
 			'default-role' => 'subscriber',
