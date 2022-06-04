2caf49a36e61ffa7bd6d0d1a3c51ca372212229e
Clayton Collie <clayton.collie@gmail.com>
2022-06-03 22:16:37 -0500

    briefs only

--- includes/classes/Structure/Archive.php
+++ includes/classes/Structure/Archive.php
@@ -21,10 +21,31 @@ class Archive {
 	 * @return void
 	 */
 	public function setup() {
+		add_action( 'pre_get_posts', [ $this, 'posts_page_category' ] );
 		add_filter( 'get_the_content_more_link', [ $this, 'read_more_link' ] );
 		add_filter( 'the_content_more_link', [ $this, 'read_more_link' ] );
 		// Remove featured image.
 		remove_action( 'genesis_entry_content', 'genesis_do_post_image', 8 );
+	}
+
+	/**
+	 * Limit the blog roll to only show a specific category.
+	 *
+	 * @param \WP_Query $query WordPress Query.
+	 *
+	 * @return void
+	 */
+	public function posts_page_category( \WP_Query $query ) {
+		if ( $query->is_admin() ) {
+			return;
+		}
+		if ( ! $query->is_main_query() ) {
+			return;
+		}
+		if ( ! $query->is_home() ) {
+			return;
+		}
+		$query->set( 'cat', '1' );
 	}
 
 	/**
@@ -39,5 +60,6 @@ class Archive {
 		$more_link_text = \str_replace( [ '[', ']', '...' ], '', $more_link_text );
 		return \str_replace( 'more-link', 'button', $more_link_text );
 	}
+
 
 }
