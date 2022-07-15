c4f14f2e016e6b26ed0eb0e0779881561a2889d6
Clayton Collie <clayton.collie@gmail.com>
2022-07-15 15:39:56 -0500

    Disable welcome email

--- includes/classes/WordPress/Overrides.php
+++ includes/classes/WordPress/Overrides.php
@@ -27,6 +27,18 @@ class Overrides {
 		add_filter( 'enable_update_services_configuration', '__return_false' );
 		// Remove language dropdown on login screen.
 		add_filter( 'login_display_language_dropdown', '__return_false' );
+		// Disable welcome email.
+		add_action( 'init', [ $this, 'disable_welcome_email' ] );
+	}
+
+	/**
+	 * Disables the Welcome email
+	 *
+	 * @return void
+	 */
+	public function disable_welcome_email() {
+		remove_action( 'register_new_user', 'wp_send_new_user_notifications' );
+		remove_action( 'edit_user_created_user', 'wp_send_new_user_notifications', 10, 2 );
 	}
 
 }
